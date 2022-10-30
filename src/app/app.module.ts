7ec68e1def0bc85455fa1d238e51ca4791780efd
MaciejNawrot <maciejnawrot.dev@gmail.com>
2022-10-30 22:39:26 +0100

    build init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './feature/landing/landing.component';
+import { ParallaxDirective } from './common/directives/parallax.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ParallaxDirective,
   ],
   imports: [
     BrowserModule,
